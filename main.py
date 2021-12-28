6569f0bb7fdfc7b954d89e8878ba31b7208fa7b6
SazidAhmed <sazidahmed.official@gmail.com>
2021-12-28 21:57:53 +0600

    working on deposit

--- main.py
+++ main.py
@@ -25,6 +25,52 @@ DEPOSITS = database['deposits']
 REGISTRATIONS = database['registrations']
 USERS = database['users']
 
+
+def check_deposits():
+    """
+    Fetch bank transactions from bank
+    Insert new deposits into database
+    """
+
+    next_url = (
+        f'{BANK_PROTOCOL}://{BANK_IP}/bank_transactions'
+        f'?recipient={BOT_ACCOUNT_NUMBER}'
+        f'&ordering=-block__created_date'
+    )
+
+    while next_url:
+        data = fetch(url=next_url, headers={})
+        bank_transactions = data['results']
+        next_url = data['next']
+
+        for bank_transaction in bank_transactions:
+
+            try:
+                DEPOSITS.insert_one({
+                    '_id': bank_transaction['id'],
+                    'amount': bank_transaction['amount'],
+                    'block_id': bank_transaction['block']['id'],
+                    'confirmation_checks': 0,
+                    'is_confirmed': False,
+                    'memo': bank_transaction['memo'],
+                    'sender': bank_transaction['block']['sender']
+                })
+            except DuplicateKeyError:
+                break
+
+
+
+@tasks.loop(seconds=5.0)
+async def poll_blockchain():
+    """
+    Poll blockchain for new transactions/deposits sent to the bot account
+    Only accept confirmed transactions
+    """
+
+    print('Polling blockchain...')
+    check_deposits()
+    check_confirmations()
+
 @bot.event
 async def on_ready():
     """
